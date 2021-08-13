1ec8dcf1ff2c94eabdca9c332924696c68827b6b
Mikkel Kidde Thomsen <45518395+THEmKidde@users.noreply.github.com>
2021-08-13 12:29:51 +0200

    Added text about the amount of alive cells, and the current generation.

--- src/GameOfLife.java
+++ src/GameOfLife.java
@@ -5,14 +5,24 @@ import java.awt.event.ActionListener;
 
 public class GameOfLife extends JPanel implements ActionListener {
 
+    int xOffset = 0;
+    int yOffset = 28;
+
     int cellSize = 14;
     int cellPadding = 1;
-    int columns = 50;
+
+    int columns = 80;
     int rows = 50;
     Cell[][] currentGeneration = CreateInitialGeneration(columns,rows);
 
+    int width = (cellSize + cellPadding) * columns - cellPadding + xOffset;
+    int height = (cellSize + cellPadding) * rows - cellPadding + yOffset;
+
+    int cellCount;
+    int generationCount = 0;
+
     public GameOfLife() {
-        this.setPreferredSize(new Dimension((cellSize + cellPadding) * columns - cellPadding,(cellSize + cellPadding) * rows - cellPadding));
+        this.setPreferredSize(new Dimension(width,height));
         this.setBackground(Color.LIGHT_GRAY);
 
         Timer timer = new Timer(100, this);
@@ -22,6 +32,10 @@ public class GameOfLife extends JPanel implements ActionListener {
     public void paint(Graphics g) {
         super.paint(g);
         Graphics2D g2d = (Graphics2D) g;
+
+        g2d.setFont(new Font("Comic Sans", Font.BOLD, 15));
+        g2d.drawString("Generation: " + generationCount, 10, 20);
+        g2d.drawString("Alive cells: " + cellCount, width - 120, 20);
 
         // Paint cells
         for (Cell[] cols : currentGeneration) {
@@ -40,6 +54,7 @@ public class GameOfLife extends JPanel implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         // Define logic for new paint
         currentGeneration = CreateNextGeneration(currentGeneration);
+        generationCount++;
         repaint();
     }
 
@@ -48,7 +63,7 @@ public class GameOfLife extends JPanel implements ActionListener {
         Cell[][] initialGrid = new Cell[cols][rows];
         for (int i = 0; i < cols; i++) {
             for (int j = 0; j < rows; j++) {
-                initialGrid[i][j] = new Cell((cellSize + cellPadding) * i, (cellSize + cellPadding) * j, (Math.random() < 0.3));
+                initialGrid[i][j] = new Cell(((cellSize + cellPadding) * i) + xOffset, ((cellSize + cellPadding) * j) + yOffset, (Math.random() < 0.3));
             }
         }
         return initialGrid;
@@ -58,6 +73,7 @@ public class GameOfLife extends JPanel implements ActionListener {
     private Cell[][] CreateNextGeneration(Cell[][] currentGeneration) {
         // Create an empty next generation
         Cell[][] nextGeneration = new Cell[columns][rows];
+        cellCount = 0;
 
         // Any live cell with two or three live neighbours survives
         // Any dead cell with three live neighbours becomes a live cell
@@ -71,8 +87,10 @@ public class GameOfLife extends JPanel implements ActionListener {
                 int neighbors = CheckNeighbors(this.currentGeneration, i, j);
                 if (this.currentGeneration[i][j].alive && (neighbors == 2 || neighbors == 3)) {
                     nextGeneration[i][j].alive = true;
+                    cellCount++;
                 } else if (!this.currentGeneration[i][j].alive && neighbors == 3) {
                     nextGeneration[i][j].alive = true;
+                    cellCount++;
                 } else {
                     nextGeneration[i][j].alive = false;
                 }
@@ -81,6 +99,7 @@ public class GameOfLife extends JPanel implements ActionListener {
         return nextGeneration;
     }
 
+    /* Check the amount of alive neighbors of the cell */
     private int CheckNeighbors(Cell[][] generation, int col, int row) {
         int neighbors = 0;
         for (int i = -1; i <= 1; i++) {
